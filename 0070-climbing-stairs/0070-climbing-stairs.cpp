07a455dce40c671658c9aa3afe6793e897a6e7ba
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-05-31 15:43:05 +0200

    Time: 0 ms (100%), Space: 8 MB (57.29%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int climbStairs_DP(int n) {
+        vector<int> stairs(n+2);
+        stairs[0]=1;
+        for(int i = 0; i < n; ++i )
+        {
+            stairs[i+1] += stairs[i];
+            stairs[i+2] += stairs[i];
+        }
+        return stairs[n];
+    }
+    int climbStairs(int n) {
+        if (n <= 1)
+            return 1;
+
+        int step_old = 1;  // ways to reach step 0
+        int step_prev = 1;  // ways to reach step 1
+
+        for (int i = 2; i <= n; ++i) {
+            int step_curr = step_old + step_prev;
+            step_old = step_prev;
+            step_prev = step_curr;
+        }
+
+        return step_prev;
+    }
+};
