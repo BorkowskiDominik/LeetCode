f06483455d58d7469ed8d6de34c92c56ece85d9b
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-02 00:43:13 +0200

    Time: 0 ms (100%), Space: 15.1 MB (25.11%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -17,15 +17,12 @@ public:
         ListNode* ptr = &dummy;
         ListNode* prev_to_removed = &dummy;
 
-        auto f = [&](){ cout << "prev: " << (prev_to_removed ? to_string(prev_to_removed->val) : "null") << " ptr: " << (ptr ? to_string(ptr->val) : "null") << endl; };
-        
-        for (int i = 0; i <= n; ++i)
+        for ( int i = 0; i <= n; ++i )
             ptr = ptr->next;
-        // f();
+
         while ( ptr ) {
             prev_to_removed = prev_to_removed->next;
             ptr = ptr->next;
-            // f();
         }
         
         ListNode* removed = prev_to_removed->next;
