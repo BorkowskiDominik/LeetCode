6db8fa7b4ca5aa27fd0c62d443667a93f5e08833
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-01 21:58:46 +0200

    Time: 5 ms (64.98%), Space: 15 MB (71.01%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,7 +17,8 @@ public:
 
 class Solution {
 public:
-    Node* copyRandomList(Node* head) {
+    // hashing solution Runtime: O(n), Memory: O(n)
+    Node* copyRandomListHashing(Node* head) {
         unordered_map<Node*, Node*> mapping;
         mapping[NULL] = NULL;
         Node* org_head = head;
@@ -40,12 +41,51 @@ public:
 
         while(copy_tail)
         {
-            copy_tail->next = mapping[org_tail->next];
+            // copy_tail->next = mapping[org_tail->next];
             copy_tail->random = mapping[org_tail->random];
             org_tail = org_tail->next;
             copy_tail = copy_tail->next;
         }
 
         return copy_head->next;
+    }
+
+    // inserting copies after originals and then splitting it back
+    // Runtime: O(n) Memory: O(1)
+    Node* copyRandomList(Node* head) {
+        if (!head) return nullptr;
+
+        // 1. Insert new nodes just after orginal
+        Node* curr = head;
+        while (curr) {
+            Node* new_node = new Node(curr->val);
+            new_node->next = curr->next;
+            curr->next = new_node;
+            curr = new_node->next;
+        }
+
+        // 2. Set random pointers (at this point copy is next element after original one)
+        curr = head;
+        while (curr) {
+            if (curr->random)
+                curr->next->random = curr->random->next;
+            curr = curr->next->next;
+        }
+
+        // 3. Separate old and new list
+        Node* new_head = head->next;
+        Node* orig = head;
+        Node* copy = new_head;
+
+        while (orig) {
+            orig->next = copy->next;
+            orig = orig->next;
+            if (orig) {
+                copy->next = orig->next;
+                copy = copy->next;
+            }
+        }
+
+        return new_head;
     }
 };
