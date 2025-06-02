fa4a06e9f6e079e5c8437a8dced4310805678548
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-02 22:04:29 +0200

    Time: 0 ms (100%), Space: 21.6 MB (92.09%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,109 @@
+class Solution {
+    using Vector2DChar = vector<vector<char>>;
+    using Iterator = vector<char>::iterator;
+
+    class MapIterator {
+        protected:
+            std::pair<int, int> _pos;
+            const Vector2DChar& _map;
+            virtual void _move_cursor() = 0;
+        public:
+            MapIterator(const Vector2DChar& map, std::pair<int, int> pos = {-1, -1}): _map(map), _pos(pos) {}
+            virtual char next() {
+                if(_pos.first < _map.size() && _pos.second < _map[_pos.first].size())
+                {
+                    char result = _map[_pos.first][_pos.second];
+                    _move_cursor();
+                    return result;
+                }
+                return '\0';
+            }
+    };
+
+    class RowIterator : public MapIterator {
+        protected:
+            virtual void _move_cursor() override {
+                _pos.second++; 
+            }
+        public:
+            RowIterator(const Vector2DChar& map, int row): MapIterator(map, {row, 0}) {}
+    };
+
+    class ColIterator : public MapIterator {
+        protected:
+            virtual void _move_cursor() override {
+                _pos.first++; 
+            }
+        public:
+            ColIterator(const Vector2DChar& map, int col): MapIterator(map, {0, col}) {}
+    };
+
+    class SquareIterator : public MapIterator {
+        std::pair<int, int> _init_pos;
+        int _element;
+    protected:
+        virtual void _move_cursor() override {
+            ++_element;
+            if (_element < 9)
+                _pos = { _init_pos.first + _element / 3, _init_pos.second + _element % 3 };
+            else
+                _pos = {10,10}; // set to invalid value
+        }
+    public:
+        SquareIterator(const Vector2DChar& map, int square)
+            : MapIterator(map), _init_pos({(square / 3) * 3, (square % 3) * 3}), _element(0) {
+            _pos = _init_pos;
+        }
+    };
+
+    bool _verify(MapIterator& it) {
+        std::bitset<10> pattern(0);
+        char value;
+        while((value = it.next())) {
+            if (value=='.') {
+                continue;
+            }
+            int i = value - '0';
+            if (pattern[i])
+                return false;
+            pattern[i] = true;
+        }
+        return true;
+    }
+
+    bool _verify_rows(Vector2DChar& board) {
+        for (int i=0; i<board.size(); ++i) {
+            auto iterator = RowIterator(board, i);
+            if(!_verify(iterator))
+                return false;
+        }
+        return true;
+    }
+
+    bool _verify_cols(Vector2DChar& board) {
+        for (int i=0; i<board.size(); ++i) {
+            auto iterator = ColIterator(board, i);
+            if(!_verify(iterator))
+                return false;
+        }
+        return true;
+
+    }
+
+    bool _verify_squares(Vector2DChar& board) {
+        for (int i=0; i<board.size(); ++i) {
+            auto iterator = SquareIterator(board, i);
+            if(!_verify(iterator))
+                return false;
+        }
+        return true;
+    }
+
+
+public:
+    bool isValidSudoku(Vector2DChar& board) {
+        if (_verify_rows(board) && _verify_cols(board) && _verify_squares(board))
+            return true;
+        return false;
+    }
+};
