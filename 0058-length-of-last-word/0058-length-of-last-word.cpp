53f53793f0d8f78aa1b972a502a458939cc3c6c1
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-02 16:31:40 +0200

    Time: 0 ms (100%), Space: 9 MB (32.04%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
-    int lengthOfLastWord(string s) {
+    int lengthOfLastWord_manual(string s) {
+        int i = s.size() - 1;
+
+        // skip tailing spaces
+        while (i >= 0 && s[i] == ' ') {
+            --i;
+        }
+
+        int length = 0;
+
+        // calculate lenght of word
+        while (i >= 0 && s[i] != ' ') {
+            --i;
+            ++length;
+        }
+
+        return length;
+    }
+
+    int lengthOfLastWord(const string& s) {
+        auto it = find_if(s.rbegin(), s.rend(), [](char c) { return c != ' '; });
+        auto end = find_if(it, s.rend(), [](char c) { return c == ' '; });
+        return distance(it, end);
+    }
+    
+    int lengthOfLastWord_STL(string s) {
         reverse(s.begin(), s.end());
         istringstream is(s);
         string w;
