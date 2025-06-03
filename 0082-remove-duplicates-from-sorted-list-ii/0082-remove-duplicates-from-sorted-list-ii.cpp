eb2736dfcf47f3f0b9c290a9f052e9fe34db97a4
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-03 16:16:57 +0200

    Time: 1 ms (16.06%), Space: 15.6 MB (93.03%) - LeetHub

---
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    void release_memory(ListNode* h) {
+        while (h) {
+            ListNode* next = h->next;
+            delete h;
+            h = next;
+        }
+        
+    }
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode dummy(777);
+        dummy.next = head;
+
+        ListNode* ptr = head;
+        ListNode* tail = &dummy;
+        
+        ListNode* remove_head = new ListNode(888);
+        ListNode* remove_tail = remove_head;
+
+        while ( ptr ) {
+            bool should_remove = (ptr->val == remove_tail->val) || (ptr->next && ptr->val == ptr->next->val);
+            if (should_remove) {
+                remove_tail->next = ptr;
+                remove_tail = ptr;
+            } else {
+                tail->next = ptr;
+                tail = ptr;
+            }
+            ptr = ptr->next;
+        }
+        remove_tail->next = nullptr;
+        tail->next = nullptr;
+        // release_memory(remove_head);
+        return dummy.next;
+    }
+};
