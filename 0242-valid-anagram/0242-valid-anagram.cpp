d96243b9258f794098cd17dacb1f17960fff2e17
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-03 23:57:54 +0200

    Time: 0 ms (100%), Space: 9.6 MB (74.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        std::array<int, 256> counter;
+        std::array<int, 26> counter {};
         if (s.size() != t.size()) return false;
 
         for (int i = 0; i < s.size(); ++i) {
-            counter[s[i]]++;
-            counter[t[i]]--;
+            counter[s[i] - 'a']++;
+            counter[t[i] - 'a']--;
         }
 
         return all_of(counter.begin(), counter.end(), [](int i){ return i == 0; });
