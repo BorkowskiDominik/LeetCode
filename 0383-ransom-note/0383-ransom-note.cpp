0fa4aea6be275fe62e7aae8bc1ee68c6d1a443ad
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-03 22:41:45 +0200

    Time: 0 ms (100%), Space: 11.6 MB (97.8%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool canConstruct(string ransomNote, string magazine) {
+    // first attempt (first idea) to solve the issue
+    bool canConstruct_first_attempt(string ransomNote, string magazine) {
         sort(ransomNote.begin(), ransomNote.end());
         sort(magazine.begin(), magazine.end());
         
@@ -15,6 +16,18 @@ public:
             m_pos++;
             r_pos++;
         }
+
+        return true;
+    }
+
+    // optimal issue
+    bool canConstruct(string ransomNote, string magazine) {
+        int freqency[26] = {}; // array to count occurences only english letters possible 
+        for (char c : magazine) 
+            freqency[c - 'a']++;
+        for (char c: ransomNote)
+            if (--freqency[c - 'a'] < 0) 
+                return false;
 
         return true;
     }
