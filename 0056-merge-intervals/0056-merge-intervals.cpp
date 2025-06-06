31a82ed86679f333c6642eced29adf988ceb6571
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-06 14:45:00 +0200

    Time: 3 ms (89.89%), Space: 23.9 MB (34.76%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,17 +5,19 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         if (intervals.size() == 1)
             return intervals;
+        
         TvvInt result;
         sort(intervals.begin(), intervals.end(), [](TvInt& a, TvInt& b) {return a[0] < b[0];});
+        
         result.push_back(intervals.front());
         for (int i = 1; i < intervals.size(); ++i) {
-            TvInt& result_back = result.back();
-            if (intervals[i][0] <= result_back[1]) {
-                result_back[1] = std::max(intervals[i][1], result_back[1]);
-            }
-            else {
-                result.push_back(intervals[i]);
-            }
+            TvInt& last = result.back();
+            TvInt& current = intervals[i];
+
+            if (current[0] <= last[1])
+                last[1] = std::max(current[1], last[1]);
+            else
+                result.push_back(current);
         }
         return result;
     }
