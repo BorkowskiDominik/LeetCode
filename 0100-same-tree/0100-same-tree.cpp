422f5cfcc243a7333b1bffaf4d0ce8df5723c70d
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-07 15:13:49 +0200

    Time: 0 ms (100%), Space: 13 MB (15.19%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -14,10 +14,34 @@
 
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
+    // recursive
+    bool isSameTree_recursive(TreeNode* p, TreeNode* q) {
         if (!p && !q) return true;
         if (!p || !q) return false;
         if (p->val != q->val) return false;
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+
+    // iterative
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        queue<TreeNode*> qp, qq;
+        qp.push(p);
+        qq.push(q);
+
+        while (!qp.empty() && !qq.empty()) {
+            TreeNode* n1 = qp.front(); qp.pop();
+            TreeNode* n2 = qq.front(); qq.pop();
+
+            if (!n1 && !n2) continue;
+            if (!n1 || !n2) return false;
+            if (n1->val != n2->val) return false;
+
+            qp.push(n1->left);
+            qp.push(n1->right);
+            qq.push(n2->left);
+            qq.push(n2->right);
+        }
+
+        return qp.empty() && qq.empty();
     }
 };
