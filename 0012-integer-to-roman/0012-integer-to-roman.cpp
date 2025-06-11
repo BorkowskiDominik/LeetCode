5357eb96bc23f9b3ee06654637dd321902ed97d4
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-12 01:18:11 +0200

    Time: 0 ms (100%), Space: 9.4 MB (65.93%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -3,6 +3,25 @@
 class Solution {
 public:
     string intToRoman(int num) {
+        static const vector<pair<int, string>> value_map = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100,  "C"}, {90,  "XC"}, {50,  "L"}, {40,  "XL"},
+            {10,   "X"}, {9,   "IX"}, {5,   "V"}, {4,   "IV"},
+            {1,    "I"}
+        };
+        
+        string result;
+        for (const auto& [value, symbol] : value_map) {
+            while (num >= value) {
+                result += symbol;
+                num -= value;
+            }
+        }
+        return result;
+    }
+
+
+    string intToRoman_manual(int num) {
         ostringstream oss; 
         
         // thousands
