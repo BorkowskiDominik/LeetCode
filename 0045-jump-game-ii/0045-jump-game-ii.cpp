1f291f68a3a2d6230f2aa62195d8b7746a4658a9
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-12 02:22:53 +0200

    Time: 0 ms (100%), Space: 20.5 MB (62.67%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -8,6 +8,23 @@ void print(const vector<T>& v) {
 }
 public:
     int jump(vector<int>& nums) {
+        int jumps = 0;
+        int farthest = 0;
+        int currEnd = 0;
+
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            farthest = max(farthest, i + nums[i]);
+
+            if (i == currEnd) {
+                ++jumps;
+                currEnd = farthest;
+            }
+        }
+
+        return jumps;
+    }
+
+    int jump_n2(vector<int>& nums) {
         if (nums.size() < 2)
             return 0;
 
