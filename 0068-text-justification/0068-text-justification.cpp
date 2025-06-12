410fe735b48aa96d9e3093e6e45eb9ccfb845de5
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-12 22:27:45 +0200

    Time: 0 ms (100%), Space: 11 MB (6.13%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    using VIter = vector<std::string>::const_iterator;
+    std::string merge_line(VIter s, VIter e, int length) {
+        int words = std::distance(s, e);
+        int letters = std::accumulate(s, e, 0, [](int sum, const std::string& e) {return sum + e.size();});
+        int total_spaces = length - letters;    
+        if (words == 1) {
+            return *s + std::string(total_spaces, ' ');
+        }
+
+        int spaces = total_spaces / (words - 1);
+        int reminder = total_spaces - spaces * (words - 1);
+
+        std::ostringstream oss;
+        oss << *s;
+        ++s;
+        while (s != e) {
+            oss << std::string(spaces + (reminder ? 1 : 0) , ' ') << *s;
+            ++s;
+            if (reminder)
+                reminder--;
+        }
+        return oss.str();
+    }
+
+    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        std::vector<std::string> result;
+        auto it_first = words.begin(), it_last = words.begin()+1;
+
+        while(it_first < words.end()) {
+            int letters = it_first->size() + 1;
+            while (it_last!= words.end() && (letters + it_last->size() <= maxWidth)) {
+                letters += it_last->size() + 1;
+                it_last++;
+            }
+            
+            // commit line
+            if (it_last != words.end()) 
+                result.push_back(merge_line(it_first, it_last, maxWidth));
+            else { // last line -> no justification
+                std::string line {*it_first};
+                while (++it_first != it_last) {
+                    line += " " + *it_first;
+                }
+                line += std::string(maxWidth - line.size(), ' ');
+                result.push_back(line);
+            }
+            it_first = it_last++;
+        }
+        return result;
+    }
+};
