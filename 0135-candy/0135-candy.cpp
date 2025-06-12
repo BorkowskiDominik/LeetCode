1e15f76294593a6699ee3758e65903e86629e734
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-12 03:51:20 +0200

    Time: 0 ms (100%), Space: 22.8 MB (80.3%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    enum class TREND {
+        UP,
+        DOWN
+    };
+    template<typename T>
+    void print(const vector<T>& v) {
+        for (auto const& e : v)
+            cout << e << " ";
+        cout << endl;
+    }
+public:
+    int candy(vector<int>& ratings) {
+        if (ratings.size() == 1) return 1;
+        if (ratings.size() == 2) return ratings[0] == ratings[1] ? 2 : 3;
+
+        std::vector<int> candies(ratings.size());
+
+        candies[0] = 1;
+        for (int r = 1; r < ratings.size(); r++) {
+            if (ratings[r] > ratings[r-1])
+                candies[r] = candies[r-1] + 1;
+            else
+                candies[r] = 1;
+        }
+        // print(candies);
+
+        for (int r = ratings.size()-2; r >= 0; r--) {
+            if (ratings[r] > ratings[r+1])
+                candies[r] = std::max(candies[r], candies[r+1]+1);
+        }
+        // print(candies);
+        return std::reduce(candies.begin(), candies.end());
+    }
+};
