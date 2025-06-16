2dfb6fc9e1102501491d8b095a357e3c86868170
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-16 23:37:29 +0200

    Time: 3 ms (68.72%), Space: 11.9 MB (78.91%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) return nullptr;
+        std::vector<Node*> val_to_new_node(101, nullptr);
+        std::vector<bool> visited(101, false);
+        std::vector<Node*> que;
+        que.push_back(node);
+        while(!que.empty()) {
+            auto old = que.back(); que.pop_back();
+            if (!val_to_new_node[old->val]) {
+                val_to_new_node[old->val] = new Node(old->val);
+            }
+            if (!visited[old->val]){
+                visited[old->val] = true;
+                std::vector<Node*> neighbors; 
+                for (const auto& n : old->neighbors) {
+                    if (!val_to_new_node[n->val]) {
+                        val_to_new_node[n->val] = new Node(n->val);
+                    }
+                    neighbors.push_back(val_to_new_node[n->val]);
+                    que.push_back(n);
+                }
+                val_to_new_node[old->val]->neighbors = std::move(neighbors);
+            }
+        }
+        return val_to_new_node[node->val];
+    }
+};
