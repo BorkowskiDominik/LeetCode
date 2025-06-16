f19486a55cb8da4592bb74f918ad0eb7195754cd
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-16 23:03:51 +0200

    Time: 0 ms (100%), Space: 14.2 MB (79.52%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -7,8 +7,7 @@ class Solution {
 
 public:
 
-    void explore(TCoords coords, TVVc& board, TVVb& visited) {
-        if (visited[coords.first][coords.second]) return;
+    void explore(TCoords coords, TVVc& board) {
         if (board[coords.first][coords.second] == 'X') return;
     
         auto is_valid_coords = [&board](int x, int y) {
@@ -16,36 +15,38 @@ public:
         };
         std::vector<TCoords> que;
         que.push_back(coords);        
-        visited[coords.first][coords.second] = true;
+        board[coords.first][coords.second] = 'S';
         while(!que.empty()) {
             auto [x, y] = que.back(); que.pop_back();
             for (auto [nx, ny] : TArrNeighbour{{{x+1, y},{x-1, y},{x, y+1},{x, y-1}}}) {
-                if (is_valid_coords(nx, ny) && !visited[nx][ny] && board[nx][ny] == 'O') {
+                if (is_valid_coords(nx, ny) && board[nx][ny] == 'O') {
                     que.push_back({nx, ny});
-                    visited[nx][ny] = true;
+                    board[nx][ny] = 'S';
                 }
             }
         }
     }
 
     void solve(TVVc& board) {
-        TVVb visited(board.size(), vector<bool>(board.front().size(), false));
+        // TVVb visited(board.size(), vector<bool>(board.front().size(), false));
         // explore fields that are on the edge
         int rows = board.size(), cols = board.front().size();
 
         for (int i = 0; i < rows; ++i) {
-            explore({i, 0     }, board, visited);
-            explore({i, cols-1}, board, visited);
+            explore({i, 0     }, board);
+            explore({i, cols-1}, board);
         }
         for (int j = 1; j < cols-1; ++j) {
-            explore({0,      j}, board, visited);
-            explore({rows-1, j}, board, visited);
+            explore({0,      j}, board);
+            explore({rows-1, j}, board);
         }
         // zero all other areas with "O" that has not been visited => so it's surrounded
-        for (int i = 1; i < rows - 1; ++i) {
-            for (int j = 1; j < cols -1; ++j) {
-                if (!visited[i][j] && board[i][j] == 'O')
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (board[i][j] == 'O')
                     board[i][j] = 'X';
+                else if (board[i][j] == 'S')
+                    board[i][j] = 'O';
             }
         } 
     }
