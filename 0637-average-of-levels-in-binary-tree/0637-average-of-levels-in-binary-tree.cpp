28e8761d9f278fadc6853bfa6e3f6a6b87262d81
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-16 04:39:48 +0200

    Time: 0 ms (100%), Space: 23.8 MB (75.48%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
@@ -10,31 +10,28 @@
  * };
  */
 class Solution {
-    std::queue<std::pair<TreeNode*, int>> _queue;
 public:
     vector<double> averageOfLevels(TreeNode* root) {
         if (!root) return {};
+        std::queue<TreeNode*> queue;
         vector<double> result;
 
         int current_level = 0;
         int level_nodes = 0;
         double level_sum = 0;
-        _queue.push({root, current_level});
-        while(!_queue.empty()) {
-            auto [node, level] = _queue.front(); _queue.pop();
-            if (!node) continue;
-            if (level != current_level) {
-                result.push_back(level_sum / level_nodes);
-                level_nodes = 0;
-                level_sum = 0;
-                current_level = level;
+
+        queue.push(root);
+        while (!queue.empty()) {
+            int level_size = queue.size();
+            double sum = 0;
+            for (int i = 0; i < level_size; ++i) {
+                TreeNode* node = queue.front(); queue.pop();
+                sum += node->val;
+                if (node->left) queue.push(node->left);
+                if (node->right) queue.push(node->right);
             }
-            level_sum += node->val;
-            level_nodes++;
-            _queue.push({node->left, current_level+1});
-            _queue.push({node->right, current_level+1});
+            result.push_back(sum / level_size);
         }
-        result.push_back(level_sum / level_nodes);
         return result;   
     }
 };
