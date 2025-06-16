70c53091960943fabd802adf18bff2abeaf0fbd9
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-16 03:14:07 +0200

    Time: 0 ms (100%), Space: 33.1 MB (12.47%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,9 +9,16 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-    unordered_map<int, int> inorder_map;
 
+#define FAST
+
+class Solution {
+    static constexpr int OFFSET = 3000;
+#ifdef FAST
+        std::vector<int> inorder_index;
+#else
+        unordered_map<int, int> inorder_map;
+#endif
     struct SubtreeBounds {
         size_t pre_begin;
         size_t pre_end;
@@ -20,29 +27,42 @@ class Solution {
     };
 
 public:
-    TreeNode* buildTree(SubtreeBounds idx, const vector<int>* preorder) {
+    #ifdef FAST        
+        Solution() : inorder_index(6001, -1) {}
+    #endif
+
+    TreeNode* buildTree(SubtreeBounds idx, const vector<int>* p_preorder) {
         if (idx.pre_begin > idx.pre_end || idx.in_begin > idx.in_end) 
             return nullptr;
 
-        size_t rootVal = (*preorder)[idx.pre_begin];
+        size_t rootVal = (*p_preorder)[idx.pre_begin];
         
         TreeNode* root = new TreeNode(rootVal);
+#ifdef FAST
+        size_t inRootIndex = inorder_index[rootVal + OFFSET];
+#else
         size_t inRootIndex = inorder_map[rootVal];
+#endif
         size_t leftSize = inRootIndex - idx.in_begin;
 
-        root->left = buildTree({idx.pre_begin + 1, idx.pre_begin + leftSize, idx.in_begin, inRootIndex - 1}, preorder);
-        root->right = buildTree({idx.pre_begin + leftSize + 1, idx.pre_end, inRootIndex +1, idx.in_end}, preorder);
+        root->left = buildTree({idx.pre_begin + 1, idx.pre_begin + leftSize, idx.in_begin, inRootIndex - 1}, p_preorder);
+        root->right = buildTree({idx.pre_begin + leftSize + 1, idx.pre_end, inRootIndex +1, idx.in_end}, p_preorder);
 
         return root;
     }
 
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+#ifdef FAST
+    for (int i = 0; i < inorder.size(); ++i) {
+        inorder_index[inorder[i] + OFFSET] = i;
+    }
+#else
         inorder_map.clear();
         inorder_map.reserve(inorder.size());
         for (int i = 0; i < inorder.size(); i++) {
             inorder_map[inorder[i]] = i;
         }
-
+#endif
         auto result = buildTree({0, preorder.size() - 1, 0, inorder.size() - 1}, &preorder);
         return result;
     }
