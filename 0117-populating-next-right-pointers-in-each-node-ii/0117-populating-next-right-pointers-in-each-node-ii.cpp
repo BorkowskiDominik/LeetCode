1d4cbe6808587f8bbf8373077a72ccac4d374f18
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-16 12:43:59 +0200

    Time: 3 ms (99.1%), Space: 18.8 MB (53.51%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -15,10 +15,56 @@ public:
         : val(_val), left(_left), right(_right), next(_next) {}
 };
 */
+enum class METHOD: int {
+    QUEUE,
+    DUMMY_NODE
+};
+
+constexpr METHOD ALGORITHM = METHOD::DUMMY_NODE;
+
 
 class Solution {
 public:
-    Node* connect(Node* root) {
+    Node* connect(Node* root) { 
+        if constexpr(ALGORITHM == METHOD::QUEUE) {
+            return connect_queue(root);
+        } else if constexpr(ALGORITHM == METHOD::DUMMY_NODE) {
+            return connect_dummy(root);
+        } else {
+            static_assert([](){return false;}, "Select proper execution mode!");
+            return root;
+        }
+    }
+
+    Node* connect_dummy(Node* root) {
+        if (!root) return nullptr;
+        Node* leftmost = root;
+
+        while (leftmost) {
+            Node* dummy = new Node(0);
+            Node* tail = dummy;
+            Node* curr = leftmost;
+
+            while (curr) {
+                if (curr->left) {
+                    tail->next = curr->left;
+                    tail = tail->next;
+                }
+                if (curr->right) {
+                    tail->next = curr->right;
+                    tail = tail->next;
+                }
+                curr = curr->next;
+            }
+
+            leftmost = dummy->next;
+            delete dummy;
+        }
+
+        return root;
+    }
+
+    Node* connect_queue(Node* root) {
         if(!root) return root;
 
         std::queue<Node*> _queue; 
@@ -27,8 +73,10 @@ public:
             int level_size = _queue.size();
             for ( int i = 0; i < level_size; ++i) {
                 auto current = _queue.front(); _queue.pop();
-                auto n = _queue.front();
-                current->next = (i != level_size -1) ? n  : nullptr;
+                if (i < level_size - 1)
+                    current->next = _queue.front();
+                else
+                    current->next = nullptr;
                 if (current->left) _queue.push(current->left);
                 if (current->right) _queue.push(current->right);
             }
