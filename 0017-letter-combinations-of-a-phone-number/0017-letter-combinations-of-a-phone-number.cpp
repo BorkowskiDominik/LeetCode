b263325ae29abf9d201cd9e93b64c014fdde85d4
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-20 00:11:05 +0200

    Time: 3 ms (7.36%), Space: 9.3 MB (58.17%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,39 @@
+
+unordered_map<char, std::vector<char>> button_to_letters {
+    {'1', {}},
+    {'2',{'a', 'b', 'c'}},
+    {'3',{'d', 'e', 'f'}},
+    {'4',{'g', 'h', 'i'}},
+    {'5',{'j', 'k', 'l'}},
+    {'6',{'m', 'n', 'o'}},
+    {'7',{'p', 'q', 'r', 's'}},
+    {'8',{'t', 'u', 'v'}},
+    {'9',{'w', 'x', 'y', 'z'}},
+    {'0', {}}
+};
+
+class Solution {
+public:
+    void build_word(string& word, const string& digits, std::vector<std::string>& result) {
+        if (digits.empty()){
+            result.push_back(word);
+        }
+
+        for (char l : button_to_letters[digits.front()]) {
+            word.push_back(l);
+            build_word(word, digits.substr(1, digits.size()-1), result);
+            word.pop_back();
+        }
+
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
+        std::vector<std::string> result;
+        std::string word;
+        word.reserve(digits.size());
+        build_word(word, digits, result);
+
+        return result;
+    }
+};
