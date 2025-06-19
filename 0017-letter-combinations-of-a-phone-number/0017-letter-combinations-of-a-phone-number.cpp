59c6e385630358e84e9a72e15e744fcb15695fa7
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-20 00:39:22 +0200

    Time: 0 ms (100%), Space: 8.9 MB (95.32%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,5 +1,10 @@
+enum class METHOD {
+    RECURSIVE,
+    ITERATIVE
+};
+constexpr METHOD ALGO = METHOD::ITERATIVE;
 
-unordered_map<char, std::vector<char>> button_to_letters {
+const static unordered_map<char, std::vector<char>> button_to_letters {
     {'1', {}},
     {'2',{'a', 'b', 'c'}},
     {'3',{'d', 'e', 'f'}},
@@ -12,6 +17,7 @@ unordered_map<char, std::vector<char>> button_to_letters {
     {'0', {}}
 };
 
+
 class Solution {
 public:
     void build_word(string& word, const string& digits, std::vector<std::string>& result) {
@@ -19,21 +25,47 @@ public:
             result.push_back(word);
         }
 
-        for (char l : button_to_letters[digits.front()]) {
+        for (char l : button_to_letters.at(digits.front())) {
             word.push_back(l);
             build_word(word, digits.substr(1, digits.size()-1), result);
             word.pop_back();
         }
-
     }
 
-    vector<string> letterCombinations(string digits) {
-        if (digits.empty()) return {};
+    vector<string> letterCombinationsRecursive(string digits) {
         std::vector<std::string> result;
         std::string word;
         word.reserve(digits.size());
         build_word(word, digits, result);
+        return result;
+    }
+
+    vector<string> letterCombinationsIterative(string digits) {
+        if (digits.empty()) return {};
+
+        vector<string> result = {""};
+
+        for (char digit : digits) {
+            vector<string> temp;
+            for (const string& prefix : result) {
+                for (char letter : button_to_letters.at(digit)) {
+                    temp.push_back(prefix + letter);
+                }
+            }
+            result = std::move(temp);
+        }
 
         return result;
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
+        if constexpr(ALGO == METHOD::ITERATIVE) {
+            return letterCombinationsIterative(digits);
+        } else if constexpr(ALGO == METHOD::RECURSIVE) {
+            return letterCombinationsRecursive(digits);
+        } else {
+            return {}; // return empty vector, algorithm not selected
+        }
     }
 };
