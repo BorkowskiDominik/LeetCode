3c8fadf617486c3f4469f8c3b7404e104cb99aed
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-20 16:32:51 +0200

    Time: 0 ms (100%), Space: 12.2 MB (16.28%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -1,6 +1,6 @@
 struct Node {
     std::string id;
-    std::vector<std::pair<std::string, double>> neighbours;
+    std::vector<std::pair<std::string, double>> neighbors;
 };
 
 class Solution {
@@ -17,8 +17,8 @@ class Solution {
             const auto& denominator = equations[i][1];
             if (!graph_map.contains(numerator)) graph_map[numerator] = Node {numerator, {}};
             if (!graph_map.contains(denominator)) graph_map[denominator] = Node {denominator, {}};
-            graph_map[numerator].neighbours.push_back({denominator,  values[i]});
-            graph_map[denominator].neighbours.push_back({numerator, 1 / values[i]});
+            graph_map[numerator].neighbors.push_back({denominator,  values[i]});
+            graph_map[denominator].neighbors.push_back({numerator, 1 / values[i]});
         }
         return graph_map;
     }
@@ -30,7 +30,7 @@ class Solution {
             double& factor) {
         if (start == end) return true;
         visited.insert(start);
-        for (const auto&[id, cost] : graph_map.at(start).neighbours) {
+        for (const auto&[id, cost] : graph_map.at(start).neighbors) {
             if (visited.contains(id)) continue;
             factor *= cost;
             if (dfs(id, end, graph_map, visited, factor)) return true;
@@ -39,13 +39,36 @@ class Solution {
         visited.erase(start);
         return false;
     }
+    
+    bool bfs(const std::string& start,
+            const std::string& end,
+            const unordered_map<std::string, Node>& graph_map,
+            std::unordered_set<std::string>& visited,
+            double& factor) {
+
+        std::queue<std::pair<std::string, double>> bfs_que;
+        bfs_que.emplace(start, 1.0);
+        visited.insert(start);
+        while (!bfs_que.empty()) {
+            auto [current, value] = bfs_que.front(); bfs_que.pop();
+            for (const auto& [neighbor, weight] : graph_map.at(current).neighbors) {
+                if (visited.contains(neighbor)) continue;
+                factor = value * weight;
+                if (neighbor == end) return true;
+                bfs_que.emplace(neighbor, factor);
+                visited.insert(neighbor);
+            }
+        }
+        return false;
+    }
 
     double evaluate(const TV<string>& query, const unordered_map<std::string, Node>& graph_map) {
         if (!graph_map.contains(query[0]) || !graph_map.contains(query[1])) return -1;
         double result = 1;
         if (query[0] == query[1]) return 1.0;
         std::unordered_set<std::string> visited {};
-        if (!dfs(query[0], query[1], graph_map, visited, result)) return -1;
+        // if (!dfs(query[0], query[1], graph_map, visited, result)) return -1;
+        if (!bfs(query[0], query[1], graph_map, visited, result)) return -1;
         return result;
     }
 
