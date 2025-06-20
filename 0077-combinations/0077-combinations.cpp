77307015af86f2304c79442a9693635928574827
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-20 02:59:20 +0200

    Time: 31 ms (98.77%), Space: 55.2 MB (99.82%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -13,9 +13,19 @@ public:
         }
     }
 
+    size_t fact(size_t k) {
+        static unordered_map<size_t, size_t> cache;
+        if (cache.contains(k)) return cache[k];
+        if (k < 2) return 1;
+        cache[k] = fact(k-1) * k;
+        return cache[k];
+    }
+
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> result;
+        result.reserve( fact(static_cast<size_t>(n)) /( fact(static_cast<size_t>(k)) * fact(static_cast<size_t>(n-k)) ) );
         vector<int> current;
+        current.reserve(k);
         backtrack(1, n, k, current, result);
         return result;
     }
