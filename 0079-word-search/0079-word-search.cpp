50ae6d5a57e3713ecdeea1867aaf6c757575dc9d
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-20 04:10:06 +0200

    Time: 31 ms (98.2%), Space: 10.8 MB (63.04%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -3,7 +3,8 @@ class Solution {
 
     bool backtrack(int x, int y, int idx,
                    const vector<vector<char>>& board,
-                   vector<vector<bool>>& visited,
+                //    vector<vector<bool>>& visited,
+                   vector<vector<uint8_t>>& visited,
                    const string& word) {
         if (idx == word.size()) return true;
 
@@ -17,19 +18,33 @@ class Solution {
             if (visited[nx][ny] || board[nx][ny] != word[idx])
                 continue;
 
-            visited[nx][ny] = true;
+            visited[nx][ny] = 1;
             if (backtrack(nx, ny, idx + 1, board, visited, word))
                 return true;
-            visited[nx][ny] = false;
+            visited[nx][ny] = 0;
         }
 
         return false;
     }
 
+    bool canBuild(const vector<vector<char>>& board, const string& word) {
+        unordered_map<char, int> board_count;
+        for (const auto& row : board)
+            for (char c : row)
+                board_count[c]++;
+        for (char c : word){
+            if(--board_count[c] < 0)
+                return false;
+        }        
+        return true;
+    }
+
 public:
     bool exist(vector<vector<char>>& board, const string& word) {
+        if (!canBuild(board, word)) return false;
         int rows = board.size(), cols = board[0].size();
-        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+        // vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+        vector<vector<uint8_t>> visited(rows, vector<uint8_t>(cols, 0));
 
         for (int i = 0; i < rows; ++i)
             for (int j = 0; j < cols; ++j)
