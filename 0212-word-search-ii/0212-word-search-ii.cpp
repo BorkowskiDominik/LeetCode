611aebaf53d0d7d628e2512905d7ca00b0f40282
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-21 22:32:44 +0200

    Time: 55 ms (97.95%), Space: 19.1 MB (72.04%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,10 +1,9 @@
-
 class Trie {
 public:
     struct Node {
         std::array<std::unique_ptr<Node>, 26> children;
         bool is_terminal = false;
-        Node* get(char c) { return children[c-'a'].get(); }
+        inline Node* get(char c) { return children[c-'a'].get(); }
         void remove(char c) { children[c-'a'] = nullptr; }
     };
 
@@ -25,6 +24,7 @@ public:
         n->is_terminal = true;
     }
     
+    [[maybe_unused]]
     bool search(const string& word) const {
         Node* n = head.get();
         for (char c: word) {
@@ -35,6 +35,7 @@ public:
         return n->is_terminal;
     }
     
+    [[maybe_unused]]
     bool startsWith(const string& prefix) const {        
         Node* n = head.get();
         for (char c : prefix) {
@@ -45,10 +46,11 @@ public:
         return true;
     }
 
-    Node* get_node(char c) {
+    inline Node* get_node(char c) {
         return head->get(c);
     }
 };
+
 
 struct Point {
     int x;
@@ -56,89 +58,118 @@ struct Point {
     bool operator==(const Point&) const = default;
 };
 
+#if 0
 namespace std {
     template<>
-    struct hash<Point> {
+    struct [[deprecated("Created for set purposed - no needed anymore")]] hash<Point> {
         std::size_t operator()(const Point& s) const noexcept {
             return std::hash<int>{}(s.x) ^ (std::hash<int>{}(s.y) << 1);
         }
     };
 }
+#endif
 
 class Solution {
     static constexpr int MAX_WORD_LEN = 10;
     static constexpr std::array<Point, 4> DIRECTIONS = {{{-1, 0},{1, 0},{0, 1},{0, -1}}};
-private: 
-    Trie generate_trie(const vector<string>& words) {
+    
+    template<typename T>
+    using TV = vector<T>;
+
+    template<typename T>
+    using TVV = vector<vector<T>>;
+
+    Trie generate_trie(const TV<string>& words) {
         Trie trie {};
         for (const auto& word : words)
             trie.insert(word);
         return trie;
     }
 
-    inline bool check_point(const vector<vector<char>>& board, const Point& p) const {
+    inline bool check_point(const TVV<char>& board, const Point& p) const {
         return p.x >= 0 && p.y >= 0 && p.x < board.size() && p.y < board[0].size();
     }
 
     inline bool should_prune(Trie::Node* n) {
-        if (!n) return true;
-        if (n->is_terminal) return false;
-        return std::all_of(n->children.begin(), n->children.end(), [](const auto& ptr){ return !ptr;});
+        if (!n) 
+            return true;
+        if (n->is_terminal) 
+            return false;
+        return std::none_of(n->children.begin(), n->children.end(), 
+                    [](const auto& ptr){ return static_cast<bool>(ptr);}
+                );
     }
 
-    void dfs(const vector<vector<char>>& board,
-            std::unordered_set<Point>& visited,
-            std::string& current_word,
+    void dfs(const TVV<char>& board,
             const Point& pos,
+            TVV<bool>& visited,
+            std::string& current_word,
             Trie::Node* n,
-            std::vector<string>& solutions) {
-        if (!n) return;
+            TV<string>& solutions) {
+        if (!n)
+            return;
+        
         if (n->is_terminal) {
             solutions.push_back(current_word);
             n->is_terminal = false; // to avoid duplicates
         };
-        if (current_word.size() == MAX_WORD_LEN) return;
+        
+        if (current_word.size() == MAX_WORD_LEN) 
+            return;
+        
         for (const auto& dir : DIRECTIONS) {
             Point next {pos.x + dir.x, pos.y + dir.y};
-            if (!check_point(board, next) || visited.contains(next)) continue;
-            visited.insert(next);
+            
+            if (!check_point(board, next) || visited[next.x][next.y]) 
+                continue;
+            
             char c = board[next.x][next.y];
-            current_word.push_back(c);
             Trie::Node* child = n->get(c);
-            dfs(board, visited, current_word, next, child, solutions);
+            if (!child)
+                continue;
+
+            visited[next.x][next.y] = true;
+            current_word.push_back(c);            
+            dfs(board, next, visited, current_word, child, solutions);
             if (should_prune(child))
                 n->remove(c);
             current_word.pop_back();
-            visited.erase(next);
+            visited[next.x][next.y] = false;
         }
     }
 
-    vector<string> find_words(const vector<vector<char>>& board, Trie& trie) {
+    TV<string> find_words(const TVV<char>& board, Trie& trie) {
         string current_word;
-        std::unordered_set<Point> visited;
-        std::vector<string> solutions;
+        // std::unordered_set<Point> visited; 
+        // using const size vector of bools helped to dramatically reduce execution time
+        // compared to unoredered_set, because no reallocations where needed neighter any
+        // kind of inserions and deletions
+        TVV<bool> visited(board.size(), TV<bool>(board[0].size(), false));
+        TV<string> solutions;
 
         current_word.reserve(MAX_WORD_LEN);
-        visited.reserve(MAX_WORD_LEN);
         
         for (int row = 0; row < board.size(); ++row) {
             for (int col = 0; col < board[0].size(); ++col) {
                 char c = board[row][col];
                 Trie::Node* n = trie.get_node(c);
-                if (!n) continue;
+                if (!n)
+                    continue;
                 Point p {row, col};
-                visited.insert(p);
+                visited[p.x][p.y] = true;
                 current_word.push_back(c);
-                dfs(board, visited, current_word, p, n, solutions);
+                
+                dfs(board, p, visited, current_word, n, solutions);
+                
                 current_word.pop_back();
-                visited.clear();
+                visited[p.x][p.y] = false;
             }
         }
         return solutions;
     }
 
 public:
-    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+    TV<string> findWords(TVV<char>& board, TV<string>& words) {
         Trie trie = generate_trie(words);
         return find_words(board, trie);
     }
