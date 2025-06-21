c1ac49463c6619960c517e406190dd2277949ec0
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-21 02:26:26 +0200

    Time: 12 ms (93.79%), Space: 51.3 MB (22.79%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,48 @@
+class Trie {
+    struct Node {
+        std::array<std::unique_ptr<Node>, 26> children;
+        bool is_terminal;
+    };
+    std::unique_ptr<Node> head;
+public:
+    Trie(): head(std::make_unique<Node>()) {}
+    
+    void insert(string word) {
+        Node* n = head.get();
+        for (char c: word) {
+            int idx = c - 'a';
+            if (!n->children[idx])
+                n->children[idx] = std::make_unique<Node>();
+            n = n->children[idx].get();
+        }
+        n->is_terminal = true;
+    }
+    
+    bool search(const string& word) const {
+        Node* n = head.get();
+        for (char c: word) {
+            int idx = c - 'a';
+            if (!n->children[idx]) return false;
+            n = n->children[idx].get();
+        }
+        return n->is_terminal;
+    }
+    
+    bool startsWith(const string& prefix) const {        
+        Node* n = head.get();
+        for (char c : prefix) {
+            int idx = c - 'a';
+            if (!n->children[idx]) return false;
+            n = n->children[idx].get();
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
