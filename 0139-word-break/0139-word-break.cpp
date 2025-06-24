224e4a3dbbe27d4455d190c4e82f7abe462bdd3d
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-24 03:44:38 +0200

    Time: 0 ms (100%), Space: 12.9 MB (61.1%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,76 @@
+struct Trie {
+    struct Node {
+        std::array<std::unique_ptr<Node>, 26> children;
+        bool is_word = false;
+        Node* operator[](char c) {
+            int idx = c - 'a';
+            if (!children[idx])
+                children[idx] = std::make_unique<Node>();
+            return children[idx].get();
+        }
+
+        Node* at(char c) {
+            return children[c-'a'].get();
+        }
+    };
+    std::unique_ptr<Node> _head;
+
+    Trie(): _head(std::make_unique<Node>()) {}
+
+    void insert(const std::string_view& s) {
+        Node* n = _head.get();
+        for (auto c: s) {
+            n = (*n)[c];
+        }
+        n->is_word = true;
+    }
+
+    bool isPrefix(char c) const {
+        return _head->at(c);
+    }
+
+    bool isPrefix(const std::string_view& s) const {
+        Node* n = _head.get();
+        for (auto c: s) { 
+            n = n->at(c);
+            if (!n) return false;
+        }
+        return true;
+    }
+
+    bool isWord(const std::string_view& s) const {
+        Node* n = _head.get();
+        for (auto c: s) { 
+            n = n->at(c);
+            if (!n) return false;
+        }
+        return n->is_word;
+    }
+};
+
+class Solution {
+    bool canConstruct(const string_view& view, const Trie& trie, const size_t start,  std::vector<int8_t>& cache) {
+        if (cache[start] != -1) return cache[start];
+        int8_t result = 0;
+        string_view s = view.substr(start, view.size()-start);
+        if (s == "" || trie.isWord(s)) {
+            result = 1;
+        } else if (trie.isPrefix(s.front())) {
+            for (size_t i = 1; i < s.size(); ++i) {
+                if (trie.isWord(s.substr(0, i)))
+                    if (canConstruct( view, trie, start + i, cache ))
+                        result =  1;
+            }
+        }
+        cache[start] = result;
+        return cache[start];
+    }
+
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        Trie trie;
+        for_each(wordDict.begin(), wordDict.end(), [&trie](const auto& s){trie.insert(s);});
+        std::vector<int8_t> cache(s.size(), -1);
+        return canConstruct(s, trie, 0, cache);
+    }
+};
