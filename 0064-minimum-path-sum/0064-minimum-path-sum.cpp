f60bf2455928864dffc63716081f30fe8e7003b5
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-26 00:16:11 +0200

    Time: 0 ms (100%), Space: 15.5 MB (86.19%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {        
-        auto print = [](const auto& v){ for (auto e : v) cout << e << " "; cout << endl;};
+    int minPathSum(vector<vector<int>>& grid) {
         vector<int> dp = grid.front();
-
-        for (int j = 1; j < dp.size(); ++j) {
-            dp[j] += dp[j-1];
-        }
+        std::partial_sum(dp.begin(), dp.end(), dp.begin());
 
         for (int i = 1; i < grid.size(); ++i) {
-            for (int j = 0 ; j < dp.size(); ++j) {
-                if (j == 0)
-                    dp[j] = grid[i][j] + dp[j];
-                else
-                    dp[j] = grid[i][j] + std::min(dp[j-1], dp[j]);
-            }
-        }
+            dp[0] = grid[i][0] + dp[0];
 
+            for (int j = 1 ; j < dp.size(); ++j) 
+                dp[j] = grid[i][j] + std::min(dp[j-1], dp[j]);
+        }
 
         return dp.back();
     }
