5abb01838bdf5bf4a391b88c952c722ecb7a75cf
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-25 21:44:30 +0200

    Time: 71 ms (75.34%), Space: 131.5 MB (31.14%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n = profits.size();
+        vector<pair<int, int>> projects;
+        for (int i = 0; i < n; ++i)
+            projects.emplace_back(capital[i], profits[i]);
+        sort(projects.begin(), projects.end());
+
+        priority_queue<int> available;
+        int idx = 0;
+
+        while (k--) {
+            while (idx < n && projects[idx].first <= w) {
+                available.push(projects[idx].second);
+                ++idx;
+            }
+            if (available.empty()) break;
+            w += available.top(); available.pop();
+        }
+
+        return w;
+    }
+
+};
