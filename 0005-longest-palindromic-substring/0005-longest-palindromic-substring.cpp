8d23ac7353c9472dc6a203243628eb94385eb78f
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-26 03:45:44 +0200

    Time: 4 ms (94.19%), Space: 9.3 MB (82.28%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -30,3 +30,55 @@ public:
         return s.substr(start, max_len);
     }
 };
+// Manacher's Algorithm
+// class Solution {
+// public:
+//     string longestPalindrome(string s) {
+//         if (s.empty()) return "";
+
+//         // \U0001f539 Preprocessing: dodajemy znaczniki, by ujednolicić długości parzyste i nieparzyste
+//         string t = "^";
+//         for (char c : s) {
+//             t += "#" + string(1, c);
+//         }
+//         t += "#$";  // granice
+
+//         int n = t.size();
+//         vector<int> P(n, 0); // długości promieni
+//         int C = 0, R = 0;     // środek i prawa granica
+
+//         for (int i = 1; i < n - 1; ++i) {
+//             int mirror = 2 * C - i;
+
+//             // Jeśli i znajduje się w obrębie prawego palindromu
+//             if (i < R) {
+//                 P[i] = min(R - i, P[mirror]);
+//             }
+
+//             // Próbujemy rozszerzyć palindrom wokół i
+//             while (t[i + 1 + P[i]] == t[i - 1 - P[i]]) {
+//                 P[i]++;
+//             }
+
+//             // Jeśli palindrom wychodzi poza znany zakres, aktualizujemy C i R
+//             if (i + P[i] > R) {
+//                 C = i;
+//                 R = i + P[i];
+//             }
+//         }
+
+//         // Znalezienie maksymalnego palindromu
+//         int max_len = 0;
+//         int center_index = 0;
+//         for (int i = 1; i < n - 1; ++i) {
+//             if (P[i] > max_len) {
+//                 max_len = P[i];
+//                 center_index = i;
+//             }
+//         }
+
+//         // Obliczenie pozycji w oryginalnym stringu s
+//         int start = (center_index - max_len) / 2;
+//         return s.substr(start, max_len);
+//     }
+// };
