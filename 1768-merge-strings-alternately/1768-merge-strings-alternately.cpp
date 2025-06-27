b016f719850390ea491ac9812edba764804bc731
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-28 00:13:34 +0200

    Time: 2 ms (43.03%), Space: 8.7 MB (12.5%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result;
+        const auto w1_size = word1.size(), w2_size = word2.size();
+        result.reserve(w1_size + w2_size);
+        
+        for (int i = 0; i < std::min(w1_size, w2_size); ++i) {
+            result.push_back(word1[i]);
+            result.push_back(word2[i]);
+        }
+
+        // handle tails if any
+        if (w1_size > w2_size) {
+            result.append(word1.substr(w2_size));
+        } else if (w1_size < w2_size) {
+            result.append(word2.substr(w1_size));
+        }
+        
+        return result;
+    }
+};
