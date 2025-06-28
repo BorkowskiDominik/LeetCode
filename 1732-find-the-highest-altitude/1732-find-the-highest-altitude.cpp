22062a41acd81fbb82c69db5a6b8adba279b39a2
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-06-28 15:55:52 +0200

    Time: 0 ms (100%), Space: 10.6 MB (99.81%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -4,11 +4,19 @@ public:
         int maxAltitude = 0;
         int level = 0; 
 
-        for (auto g : gain) {
-            level += g;
-            maxAltitude = std::max(maxAltitude, level);
-        }
+        std::accumulate(gain.begin(), gain.end(), 0, [&maxAltitude](int sum, int e){
+            maxAltitude = std::max(maxAltitude, sum+e);
+            return sum+e;
+        });
 
         return maxAltitude;
+
+        // manual solution: 
+        // for (auto g : gain) {
+        //     level += g;
+        //     maxAltitude = std::max(maxAltitude, level);
+        // }
+
+        // return maxAltitude;
     }
 };
