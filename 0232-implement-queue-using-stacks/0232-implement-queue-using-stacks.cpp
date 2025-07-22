ac1f116d300585e3e6d58f3e26bb8a6cd0c82119
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-07-22 22:02:50 +0200

    Time: 0 ms (100%), Space: 9.6 MB (65.57%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,45 +1,80 @@
 class MyQueue {
-    std::stack<int> s1;
-    std::stack<int> s2;
+    std::stack<int> s1, s2;
 
-    void shift_all(stack<int>& source, stack<int>& dest) {
-        while (!source.empty()){
-            dest.push(source.top());
-            source.pop();
+    void transferIfNeeded() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
         }
     }
 
 public:
-    MyQueue() {
-        
-    }
-    
     void push(int x) {
-        // Pushes element x to the back of the queue.
         s1.push(x);
     }
     
     int pop() {
-        //  Removes the element from the front of the queue and returns it.
-        shift_all(s1, s2);
-        auto res = s2.top();
+        transferIfNeeded();
+        int res = s2.top();
         s2.pop();
-        shift_all(s2, s1);
         return res;
     }
     
     int peek() {
-        // Returns the element at the front of the queue
-        shift_all(s1, s2);
-        auto res = s2.top();
-        shift_all(s2, s1);
-        return res;
+        transferIfNeeded();
+        return s2.top();
     }
     
     bool empty() {
-        return s1.empty() && s2.empty();   
+        return s1.empty() && s2.empty();
     }
 };
+
+
+// class MyQueue {
+//     std::stack<int> s1;
+//     std::stack<int> s2;
+
+//     void shift_all(stack<int>& source, stack<int>& dest) {
+//         while (!source.empty()){
+//             dest.push(source.top());
+//             source.pop();
+//         }
+//     }
+
+// public:
+//     MyQueue() {
+        
+//     }
+    
+//     void push(int x) {
+//         // Pushes element x to the back of the queue.
+//         s1.push(x);
+//     }
+    
+//     int pop() {
+//         //  Removes the element from the front of the queue and returns it.
+//         shift_all(s1, s2);
+//         auto res = s2.top();
+//         s2.pop();
+//         shift_all(s2, s1);
+//         return res;
+//     }
+    
+//     int peek() {
+//         // Returns the element at the front of the queue
+//         shift_all(s1, s2);
+//         auto res = s2.top();
+//         shift_all(s2, s1);
+//         return res;
+//     }
+    
+//     bool empty() {
+//         return s1.empty() && s2.empty();   
+//     }
+// };
 
 /**
  * Your MyQueue object will be instantiated and called as such:
