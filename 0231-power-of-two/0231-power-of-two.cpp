0cdf1183636cdacae88b42b641a8e130f3093850
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-07-22 19:48:44 +0200

    Time: 0 ms (100%), Space: 7.9 MB (53.28%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,7 +2,10 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         bitset<sizeof(int)*8> bits(n);
-        if (n >= 0) return bits.count() == 1;
+        if (n > 0) return bits.count() == 1;
         return false;
+
+        // alternative solution without conversion to bitset
+        //return n > 0 && (n & (n - 1)) == 0;
     }
 };
