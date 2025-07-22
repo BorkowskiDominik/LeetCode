fc043cfebeced2c78946ce57b56165ffbe054276
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-07-22 22:34:46 +0200

    Time: 0 ms (100%), Space: 8.6 MB (78.21%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    int sumDigits(int n) {
+        int res = 0;
+        while (n) {
+            res += n % 10;
+            n /= 10;
+        }
+        return res;
+    }
+public:
+    // int addDigits(int num) {
+    //     int sum = sumDigits(num);
+    //     while (sum > 9) {
+    //         num = sum;
+    //         sum = sumDigits(num);
+    //     }
+    //     return sum;
+    // }
+    int addDigits(int num) {
+    return num == 0 ? 0 : 1 + (num - 1) % 9;
+}
+};
