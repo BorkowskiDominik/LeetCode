8191e5c421f71459d868b6c27733f4a9d7ffa07f
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-08-04 09:45:23 +0200

    Time: 0 ms (100%), Space: 8 MB (78.56%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if (word.size() == 1) return true;
+        if (std::islower(word.at(0)) || std::islower(word.at(1)))
+            return std::all_of(
+                word.begin()+1,
+                word.end(),
+                [](unsigned char c) { return std::islower(c); }
+            );
+        return std::all_of(
+            word.begin(),
+            word.end(),
+            [](unsigned char c) { return std::isupper(c); }
+        );
+        
+    }
+};
