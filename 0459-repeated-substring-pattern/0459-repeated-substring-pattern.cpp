a6d16ab3776dbbb8ad5749c8bbf196d1caec1af7
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-08-04 18:07:57 +0200

    Time: 0 ms (100%), Space: 10.9 MB (99.44%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,3 +1,28 @@
+// KMP: (Knuth–Morris–Pratt)
+// class Solution {
+// public:
+//     bool repeatedSubstringPattern(string s) {
+//         int n = s.size();
+//         vector<int> lps(n, 0); // Longest Prefix Suffix
+//         int len = 0; // długość poprzedniego najdłuższego prefiksu-sufiksu
+//         int i = 1;
+
+//         while (i < n) {
+//             if (s[i] == s[len]) {
+//                 lps[i++] = ++len;
+//             } else if (len > 0) {
+//                 len = lps[len - 1];
+//             } else {
+//                 lps[i++] = 0;
+//             }
+//         }
+
+//         int last = lps[n - 1];
+//         return last > 0 && n % (n - last) == 0;
+//     }
+// };
+
+// My naive solution (even though 100%)
 class Solution {
     bool isBuildOf(string_view s, string_view pattern) {
         int pSize = pattern.size();
