bd675193eb00d0de6905772622b706e4124858ef
BorkowskiDominik <44896053+BorkowskiDominik@users.noreply.github.com>
2025-08-10 01:35:07 +0200

    Time: 0 ms (100%), Space: 12.1 MB (90.72%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -4,6 +4,8 @@ public:
         for (auto it = s.begin(); it < s.end(); ) {
             auto word_end = std::find(it, s.end(), ' ');
             std::reverse(it, word_end);
+            if (word_end == s.end())
+                break;
             it = std::next(word_end);
         }
         return s;
